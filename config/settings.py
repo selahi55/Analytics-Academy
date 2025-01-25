277ddd7df76f6e97d31fb4b86df5f24fc469994b
6SYNAPESE9 <shayaanelahi@gmail.com>
2025-01-25 09:52:22 +0100

    Deployment: Fixed module not found "main" error

--- config/settings.py
+++ config/settings.py
@@ -15,8 +15,6 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv(dotenv_path="C:/Users/shaya/OneDrive/Desktop/Analytics-Academy/.env")
-DJANGO_SETTINGS_MODULE = "config.settings"
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
